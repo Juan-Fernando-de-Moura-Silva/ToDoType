70dec17b5955feb4266f4f9a1a7b668f2706b0ef
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-08-16 19:50:49 -0300

    seguindo normalmente

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>To Do List</h1>
     </div>
   );
 }
